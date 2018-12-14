d11a86de5e16fa201d81010d29fb0ba3dbfa1137
Migmigr <45515149+Migmigr@users.noreply.github.com>
2018-12-15 01:25:04 +0200

    Add files via upload

---
+++ Bin/Bin/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Bin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "1.4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "c";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "R";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.LabelStyle.Format = "10^#";
+            chartArea1.AxisX.Title = "x";
+            chartArea1.AxisY.LabelStyle.Format = "\"10\"^#";
+            chartArea1.AxisY.Title = "estP(x)";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 65);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(699, 358);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "xmin";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 435);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Bin/Bin/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+namespace Bin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Part : IEquatable<Part>, IComparable<Part>
+        {
+            public string Name { get; set; }
+
+            public Decimal Count { get; set; }
+
+            public override string ToString()
+            {
+                return Regex.Replace(Name, "\\,", ".") + " " + Regex.Replace(Count.ToString(), "\\,", ".");
+            }
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+                Part objAsPart = obj as Part;
+                if (objAsPart == null) return false;
+                else return Equals(objAsPart);
+            }
+            public int SortByNameAscending(string name1, string name2)
+            {
+                return name1.CompareTo(name2);
+            }
+
+            // Default comparer for Part type.
+            public int CompareTo(Part comparePart)
+            {
+                // A null value means that this object is greater.
+                if (comparePart == null)
+                    return 1;
+                else
+                    return this.Count.CompareTo(comparePart.Count);
+            }
+            public override int GetHashCode()
+            {
+                return Convert.ToInt32(Count);
+            }
+            public bool Equals(Part other)
+            {
+                if (other == null) return false;
+                return (this.Count.Equals(other.Count));
+            }
+            // Should also override == and != operators.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MersenneTwister mt = new MersenneTwister();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            Decimal c = Convert.ToDecimal(Regex.Replace(textBox1.Text, "\\.", ","));
+            Double R = Convert.ToDouble(Regex.Replace(textBox2.Text, "\\.", ","));
+            int xmin = Convert.ToInt32(textBox3.Text);
+            List<List<string>> LI = new List<List<string>>();
+            LI.Add(Directory.GetFiles(@"New Value").ToList());
+
+            int counta = 0;
+            List<int> cof = new List<int>();
+
+            int ccc = 0;
+            int sc = 1000;
+            sc = 1;
+            for (int t = 0; t < LI[0].Count; t++)
+            {
+                List<string> tt = new List<string>();
+                List<Part> p = new List<Part>();
+                for (int j = 0; j < LI.Count; j++)
+                {
+                    StreamReader myStream = null;
+                    if ((myStream = new StreamReader(LI[j][t])) != null)
+                    {
+                        using (myStream)
+                        {
+                            string line;
+                            while ((line = myStream.ReadLine()) != null)
+                            {
+                                string[] parts = Regex.Split(line, @" ");
+                                string[] par = line.Split('\\');
+                                Double n = Convert.ToDouble(parts.Last());
+                                xmin = 0;//лишнее
+                                if (n > xmin)
+                                {
+                                    if (j != 1)
+                                    {
+                                        if (n > 0)
+                                        {
+                                            counta++;
+                                            p.Add(new Part { Name = "", Count = Convert.ToDecimal(n) * sc });
+                                        }
+                                    }
+                                    else
+                                    {
+
+                                        tt.Add(line);
+                                        if (ccc >= 0)
+                                        {
+                                            Double pa = 5000.0 / 75545.0;
+                                            if (mt.genrand_real2() <= pa)
+                                            {
+                                                cof.Add(ccc);
+                                            }
+                                            ccc++;
+                                        }
+
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+                decimal F = p.Last().Count;
+                int colvos = 0;
+                List<Part> PDF = new List<Part>();
+                List<Part> hr = new List<Part>();
+                int pos = p.FindIndex(x => x.Count == xmin);
+                int firt = p.FindIndex(x => x.Count == xmin);
+                Decimal df = p.Last().Count;
+                Decimal sum = 0;
+
+                int nb = 1;
+                Decimal bj = Math.Floor(c * (Decimal)Math.Pow(R, nb));
+                Decimal bj1 = Math.Floor(c * (Decimal)Math.Pow(R, nb - 1));
+                for (int i = 1; i <= 49; i++)
+                {
+                    hr.Add(new Part { Name = i.ToString(), Count = 0 });
+                }
+                for (int i = 0; i < p.Count; i++)
+                {
+                    hr[(int)p[i].Count - 1].Count++;
+                }
+                pos = 0;
+                firt = 0;
+                Decimal pp = p[0].Count;
+                int co = 0;
+                for (int i = 0; i < p.Count; i++)
+                {
+                    if (p[i].Count == pp)
+                    {
+                        co++;
+                    }
+                    else
+                    {
+                        colvos += co;
+                        if (co != 0)
+                        {
+                            hr.Add(new Part { Name = pp.ToString(), Count = (Convert.ToDecimal(co) / (p.Count - firt)) });
+                            sum += (Convert.ToDecimal(co) / (p.Count - firt));
+                        }
+                        pp = p[i].Count;
+                        co = 1;
+                    }
+
+                }
+                if (p[p.Count - 1].Count == pp)
+                {
+                    colvos += co;
+                    hr.Add(new Part { Name = pp.ToString(), Count = (Convert.ToDecimal(co) / (p.Count - firt)) });
+                    sum += (Convert.ToDecimal(co) / (p.Count - firt));
+                }
+
+                pos = 0;
+                Decimal co2 = 0;
+                nb = 1;
+
+                bj = Math.Floor(c * (Decimal)Math.Pow(R, nb));
+                bj1 = Math.Floor(c * (Decimal)Math.Pow(R, nb - 1));
+
+                for (int j = pos; j < hr.Count; j++)
+                {
+                    if (Convert.ToDecimal(hr[j].Name) <= Math.Max(bj - 1, bj1))
+                    {
+                        Decimal ri = 1;
+                        if (j == hr.Count - 1)
+                        {
+                            ri = (Convert.ToDecimal(hr[j].Name) - Convert.ToDecimal(hr[j - 1].Name));
+                        }
+                        else
+                        {
+                            ri = (Convert.ToDecimal(hr[j + 1].Name) - ((j == 0) ?
+                                Convert.ToDecimal(hr[j].Name) : (Convert.ToDecimal(hr[j - 1].Name)) / 2));
+                        }
+                        ri = Convert.ToDecimal(1);
+                        co2 += hr[j].Count * ri;
+                    }
+                    else
+                    {
+                        Decimal razm = bj1 * ((Decimal)R - 1);
+                        Decimal pointa = c * (Decimal)Math.Pow(R, nb - 0.5);
+                        if (co2 != 0)
+                        {
+                            PDF.Add(new Part { Name = (pointa / sc).ToString(), Count = (Convert.ToDecimal(co2) / (razm)) });
+                            chart1.Series[1].Points.AddXY(Math.Log10((Double)pointa), Math.Log10((Double)PDF.Last().Count));
+                        }
+
+                        nb++;
+                        bj1 = bj;
+                        bj = Math.Floor(c * (Decimal)Math.Pow(R, nb));
+                        j--;
+                        co2 = 0;
+                    }
+                }
+                Decimal razm1 = bj1 * ((Decimal)R - 1);
+                Decimal pointa1 = c * (Decimal)Math.Pow(R, nb - 0.5);
+                PDF.Add(new Part { Name = (pointa1 / sc).ToString(), Count = (Convert.ToDecimal(co2) / (razm1)) });
+                chart1.Series[1].Points.AddXY(Math.Log10((Double)pointa1), Math.Log10((Double)PDF.Last().Count));
+
+                StreamWriter mySt = null;
+                if ((mySt = new StreamWriter("Binned" + LI[0][t].Split('\\').Last())) != null)
+                {
+                    using (mySt)
+                    {
+                        foreach (Part s in PDF)
+                            mySt.WriteLine(s.ToString());
+
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
